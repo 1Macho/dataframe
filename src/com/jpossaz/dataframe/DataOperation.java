075f31e5bb24e73d184e62d94417816abe774653
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2019-05-02 22:09:06 -0500

    All statistical operations implemented.

--- src/com/jpossaz/dataframe/DataOperation.java
+++ src/com/jpossaz/dataframe/DataOperation.java
@@ -2,14 +2,77 @@
 package com.jpossaz.dataframe;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class DataOperation {
-    /*public static Double obtainAverage (DataFrame input)
+
+    public static Object obtainTrend (DataFrame input)
+    {
+        List<Object> verifiedObjects = new ArrayList<>();
+        Object currentTrend = null;
+        int currentTrendCount = 0;
+        for (Registry a : input)
+        {
+            if (!verifiedObjects.contains(a))
+            {
+                int localCount = 0;
+                for (Registry b : input)
+                {
+                    if(a.equals(b))
+                    {
+                        localCount++;
+                    }
+                }
+                if (localCount > currentTrendCount)
+                {
+                    currentTrend = a;
+                    currentTrendCount = localCount;
+                }
+                verifiedObjects.add(a);
+            }
+        }
+        return currentTrend;
+    }
+
+    public static Double obtainStandardDeviation (DataFrame input)
     {
         if(!input.watchingNumericValue())
             throw new ClassCastException("The watched value is non-numeric.");
-        
-    }*/
+        Double mean = obtainMean(input);
+        Double accumulation = 0.0d;
+        int count = 0;
+        for (Registry reg : input)
+        {
+            if (reg.obtainWatchedValue().getIsSet())
+            {
+                Double thisValue = (Double)reg.obtainWatchedValue().getValue();
+                Double delta = mean - thisValue;
+                accumulation += delta * delta;
+                count++;
+            }
+        }
+        Double variance = accumulation / count;
+        return Math.sqrt(variance);
+    }
 
+    public static Double obtainMean (DataFrame input)
+    {
+        if(!input.watchingNumericValue())
+            throw new ClassCastException("The watched value is non-numeric.");
+        Double accumulation = 0.0d;
+        int count = 0;
+        for (Registry reg : input)
+        {
+            if (reg.obtainWatchedValue().getIsSet())
+            {
+                accumulation += (Double)reg.obtainWatchedValue().getValue();
+                count++;
+            }
+        }
+        return accumulation / count;
+    }
 
     public static Double obtainMinimum (DataFrame input) throws ClassCastException
     {
